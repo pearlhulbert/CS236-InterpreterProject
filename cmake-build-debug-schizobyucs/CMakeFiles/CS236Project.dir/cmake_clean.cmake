bef8a0fcba622c36dd20a1c4f8f79c1eeb71b2a0
Pearl Kathleen Hulbert <p90782@kansas.cs.byu.edu>
2021-09-04 14:06:38 -0600

    Project 0 Complete

---
+++ cmake-build-debug-schizobyucs/CMakeFiles/CS236Project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS236Project.dir/Book.cpp.o"
+  "CMakeFiles/CS236Project.dir/BookCreator.cpp.o"
+  "CMakeFiles/CS236Project.dir/BookListCreator.cpp.o"
+  "CMakeFiles/CS236Project.dir/Info.cpp.o"
+  "CMakeFiles/CS236Project.dir/Library.cpp.o"
+  "CMakeFiles/CS236Project.dir/main.cpp.o"
+  "CS236Project"
+  "CS236Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS236Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
